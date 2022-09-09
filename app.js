650a8ab8ac3a13c6bba080444c3961b97866c689
eljoker22 <eljokermano50@gmail.com>
2022-09-09 20:12:18 +0200

    test 02

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(cors({
 
 app.use('/api/v1/auth', authRoutes);
 
-app.get('/', (req, res) => { 
-    res.send('ko back-end i do it') 
-})
+// app.get('/', (req, res) => { 
+//     res.send('ko back-end i do it') 
+// })
 
 const start = async () => {
     try{
