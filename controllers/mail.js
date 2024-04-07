57c228ee68f510d6cae29a8f5a76825ab5d7b0be
eljoker22 <eljokermano50@gmail.com>
2024-04-07 21:09:30 +0200

    remove mailer send

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,29 +1,29 @@
-const Recipient = require("mailersend").Recipient;
-const EmailParams = require("mailersend").EmailParams;
-const MailerSend = require("mailersend");
+// const Recipient = require("mailersend").Recipient;
+// const EmailParams = require("mailersend").EmailParams;
+// const MailerSend = require("mailersend");
 
 
-const mailersend = new MailerSend({
-    apiKey: "mlsn.b15d140ee1855a6f0fba8550befc41688eab44e7cea92703357930e4feff1f3e",
-});
+// const mailersend = new MailerSend({
+//     apiKey: "mlsn.b15d140ee1855a6f0fba8550befc41688eab44e7cea92703357930e4feff1f3e",
+// });
 
 const sendEamilConfermation = async (email, code) => {
-    const recipients = [new Recipient(email, "Recipient")];
+    // const recipients = [new Recipient(email, "Recipient")];
 
-    const emailParams = new EmailParams()
-    .setFrom("trial-jy7zpl99o0ol5vx6.mlsender.net")
-    .setFromName("Ko Sports")
-    .setRecipients(recipients)
-    .setSubject("verify account")
-    .setHtml(`<p>your confermation code: <strong>${code}</strong></p>`)
-    .setText("Greetings from the team, you got this message through MailerSend.");
+    // const emailParams = new EmailParams()
+    // .setFrom("trial-jy7zpl99o0ol5vx6.mlsender.net")
+    // .setFromName("Ko Sports")
+    // .setRecipients(recipients)
+    // .setSubject("verify account")
+    // .setHtml(`<p>your confermation code: <strong>${code}</strong></p>`)
+    // .setText("Greetings from the team, you got this message through MailerSend.");
 
-    const send = await  mailersend.send(emailParams);;
-    if (send) {
-        console.log(send)
-    }else{
-        console.log('error send')
-    }
+    // const send = await  mailersend.send(emailParams);;
+    // if (send) {
+    //     console.log(send)
+    // }else{
+    //     console.log('error send')
+    // }
 }
 
 // send email forgot password
